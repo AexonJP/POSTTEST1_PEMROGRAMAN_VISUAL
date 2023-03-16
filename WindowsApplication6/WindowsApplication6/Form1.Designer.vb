fb2e378aa078ee68d843c2700b7112966d2206d4
AexonJP <92908255+AexonJP@users.noreply.github.com>
2023-03-16 18:41:25 +0800

    Add files via upload

---
+++ WindowsApplication6/WindowsApplication6/Form1.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.reset = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(120, 100)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(203, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(120, 146)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(203, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"km", "hm", "dam", "m", "dm", "cm", "mm"})
+        Me.ComboBox1.Location = New System.Drawing.Point(340, 99)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(72, 21)
+        Me.ComboBox1.TabIndex = 2
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"km", "hm", "dam", "m", "dm", "cm", "mm"})
+        Me.ComboBox2.Location = New System.Drawing.Point(340, 145)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(72, 21)
+        Me.ComboBox2.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(44, 100)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Satuan"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(44, 145)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(74, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Hasil Konversi"
+        '
+        'reset
+        '
+        Me.reset.Location = New System.Drawing.Point(205, 234)
+        Me.reset.Name = "reset"
+        Me.reset.Size = New System.Drawing.Size(75, 23)
+        Me.reset.TabIndex = 6
+        Me.reset.Text = "Reset"
+        Me.reset.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(487, 325)
+        Me.Controls.Add(Me.reset)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents reset As System.Windows.Forms.Button
+
+End Class
---
+++ WindowsApplication6/WindowsApplication6/Form1.vb
@@ -0,0 +1,82 @@
+﻿Public Class Form1
+    Dim awal As Integer
+    Dim konversi As Integer
+    Dim perbedaan As Double
+
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles reset.Click
+        TextBox1.Text = ""
+        TextBox2.Text = 0
+        ComboBox1.ResetText()
+        ComboBox2.ResetText()
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
+        perbedaan = 1
+        If ComboBox1.SelectedIndex = 6 Then
+            awal = 1
+        ElseIf ComboBox1.SelectedIndex = 5 Then
+            awal = 2
+        ElseIf ComboBox1.SelectedIndex = 4 Then
+            awal = 3
+        ElseIf ComboBox1.SelectedIndex = 3 Then
+            awal = 4
+        ElseIf ComboBox1.SelectedIndex = 2 Then
+            awal = 5
+        ElseIf ComboBox1.SelectedIndex = 1 Then
+            awal = 6
+        ElseIf ComboBox1.SelectedIndex = 0 Then
+            awal = 7
+        End If
+        ok()
+    End Sub
+    Private Sub ComboBox2_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox2.SelectedIndexChanged
+        perbedaan = 1
+        If ComboBox2.SelectedIndex = 6 Then
+            konversi = 1
+        ElseIf ComboBox2.SelectedIndex = 5 Then
+            konversi = 2
+        ElseIf ComboBox2.SelectedIndex = 4 Then
+            konversi = 3
+        ElseIf ComboBox2.SelectedIndex = 3 Then
+            konversi = 4
+        ElseIf ComboBox2.SelectedIndex = 2 Then
+            konversi = 5
+        ElseIf ComboBox2.SelectedIndex = 1 Then
+            konversi = 6
+        ElseIf ComboBox2.SelectedIndex = 0 Then
+            konversi = 7
+        End If
+        ok()
+    End Sub
+
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+        ok()
+    End Sub
+    Private Sub ok()
+        perbedaan = 1
+        If awal >= konversi Then
+            perbedaan = 10 ^ (awal - konversi)
+        ElseIf awal < konversi Then
+            perbedaan = 1 / 10 ^ (konversi - awal)
+        End If
+        If TextBox1.Text = "" Then
+            TextBox2.Text = 0 * perbedaan
+        Else : TextBox2.Text = Convert.ToDouble(Convert.ToDouble(TextBox1.Text) * perbedaan)
+        End If
+        perbedaan = 1
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        perbedaan = 1
+    End Sub
+
+    
+End Class
